cc9cc198fde321e7f3879e8c7a4be3febbf210bf
Ilyat9 <107850970+Ilyat9@users.noreply.github.com>
2025-12-20 19:24:42 +0300

    Create Octagon.h

---
+++ include/Octagon.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Figure.h"
+#include <cmath>
+
+template<Scalar T>
+class Octagon : public Figure<T> {
+public:
+    Octagon(const Point<T>& center, T radius);
+    Octagon(const Octagon& other);
+    
+    double area() const override;
+    Point<T> geometricCenter() const override;
+    std::string getName() const override { return "Octagon"; }
+    bool isValid() const override;
+    
+    Octagon& operator=(const Octagon& other);
+};
