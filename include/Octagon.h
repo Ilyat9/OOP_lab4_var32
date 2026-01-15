b6ddbc188b7a6db8c8a21ebc988bb61b51c21a94
Ilya <107850970+Ilyat9@users.noreply.github.com>
2026-01-15 20:22:22 +0300

    Update Octagon.h

--- include/Octagon.h
+++ include/Octagon.h
@@ -5,13 +5,15 @@
 template<Scalar T>
 class Octagon : public Figure<T> {
 public:
-    Octagon(const Point<T>& center, T radius);
+    Octagon(const Point<T>& center, T side_len);
     Octagon(const Octagon& other);
     
     double area() const override;
     Point<T> geometricCenter() const override;
     std::string getName() const override { return "Octagon"; }
     bool isValid() const override;
+
+    void display(std::ostream& os) const override;
     
     Octagon& operator=(const Octagon& other);
 };
